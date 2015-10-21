e3b8e2526eaa9b426742111e96f494be82608dd7
Victor M. Suarez <svmnotn@gmail.com>
2015-10-20 23:16:58 -0400

    add Driver trait and some test drivers
    * created TestDriver with a test implementation
    * created driver_postgres.rs to hold the postgres driver and implemented it with a test impl

---
+++ src/drivers/driver_postgres.rs
@@ -0,0 +1,25 @@
+#![cfg(feature = "driver-postgres")]
+use drivers::Driver as Driv;
+
+pub struct Driver{
+    target: &'static str,
+}
+
+impl Driv for Driver {
+    fn new(target: &'static str) -> Self {
+        Driver { target: target }
+    }
+
+    fn get_status(&self) -> isize{
+        println!("Get Status of {}", self.target);
+        return 0
+    }
+
+    fn set_status(&mut self, status: isize){
+        println!("Set Status to \'{}\'", status);
+    }
+
+    fn execute(&self, motion: &'static str){
+        println!("I am a thing that says \'{0}\' with {1}", motion, self.target);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+mod drivers;
+
 fn main() {
     println!("Hello, world!");
 }
