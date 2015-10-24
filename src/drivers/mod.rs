5bff46d4d3887e1eb07cbec524d7edd4c10aed40
Victor M. Suarez <svmnotn@gmail.com>
2015-10-24 15:10:54 -0400

    remove TestDriver from drivers/mod.rs
    not needed since the tests mod implements it locally

--- src/drivers/mod.rs
+++ src/drivers/mod.rs
@@ -5,30 +5,5 @@ pub trait Driver{
     fn execute(&self, motion: &'static str);
 }
 
-pub struct TestDriver{
-    target: &'static str,
-    status: isize,
-}
-
-impl Driver for TestDriver {
-    fn new(target: &'static str) -> Self {
-        TestDriver { target: target, status: 0 }
-    }
-
-    fn get_status(&self) -> isize{
-        println!("Get Status of {0}, which is \'{1}\'", self.target, self.status);
-        return self.status
-    }
-
-    fn set_status(&mut self, status: isize){
-        println!("Set Status to \'{}\'", status);
-        self.status = status;
-    }
-
-    fn execute(&self, motion: &'static str){
-        println!("I am a {1} that says \'{0}\'", motion, self.target);
-    }
-}
-
 #[cfg(feature = "driver-postgres")]
 pub mod postgres;
