817eacece8616f9f7155cceb1dc9da04c3f06fc0
Victor M. Suarez <svmnotn@gmail.com>
2015-11-01 00:15:47 -0400

    update the Motion struct to match what is needed

--- src/motions.rs
+++ src/motions.rs
@@ -1,4 +1,21 @@
 pub struct Motion{
-    pub add: &'static str,
-    pub sub: &'static str,
+    pub name: String,
+    pub add_name: String,
+    pub sub_name: String,
+    pub add: String,
+    pub sub: String,
+}
+
+impl Motion {
+    pub fn get(add: String, sub: String) -> Self{
+        Motion{name:    "motion".to_string(),
+               add_name: "motion.add".to_string(),
+               sub_name: "motion.sub".to_string(),
+               add:     add,
+               sub:     sub}
+    }
+}
+
+pub fn get(directory: &String) -> Vec<Motion>{
+    return vec![Motion::get("add 1".to_string(), "sub 1".to_string())];
 }
