90d0d3bb4d755ab18d7a15a815fad44664412357
Victor M. Suarez <svmnotn@gmail.com>
2015-11-01 00:13:53 -0400

    add a default driver for when no compile objective is setup
    This should never happen and might be removed

--- src/drivers/mod.rs
+++ src/drivers/mod.rs
@@ -5,5 +5,30 @@ pub trait Driver{
     fn execute(&self, motion: &String) -> &Self;
 }
 
+pub struct DefaultDriver{
+    target: String,
+    status: isize,
+}
+
+impl Driver for DefaultDriver {
+    fn new(target: String) -> Self {
+        DefaultDriver { target: target, status: 0 }
+    }
+
+    fn get_status(&self) -> isize{
+        return self.status
+    }
+
+    fn set_status(&mut self, status: isize)-> &mut Self{
+        self.status = status;
+        self
+    }
+
+    fn execute(&self, motion: &String)-> &Self{
+        println!("Running \"{0}\" @ {1}", motion, self.target);
+        self
+    }
+}
+
 #[cfg(feature = "driver-postgres")]
 pub mod postgres;
