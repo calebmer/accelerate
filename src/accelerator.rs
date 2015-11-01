a4a6810c3302b3af556ef133ae7b1f4305687ed6
Victor M. Suarez <svmnotn@gmail.com>
2015-11-01 00:11:15 -0400

    stop my attempt to do OOP for this

--- src/accelerator.rs
+++ src/accelerator.rs
@@ -1,92 +1,71 @@
 use drivers::Driver;
 use motions::Motion;
 
-pub struct Accelerator<D: Driver> {
-    motions: Vec<Motion>,
-    driver: D,
+fn within_bounds(motions: &Vec<Motion>, n: isize) -> isize{
+    let min = 0;
+    let max = motions.len() as isize;
+    if n < min { return min }
+    if n > max { return max }
+    n
 }
 
-impl <D: Driver> Accelerator<D> {
-    pub fn new(mots: Vec<Motion>, drv: D) -> Self {
-        Accelerator{motions: mots, driver: drv}
-    }
-
-    fn within_bounds(&self, n: isize) -> isize{
-        let min = 0;
-        let max = self.motions.len() as isize;
-        if n < min { return min }
-        if n > max { return max }
-        n
-    }
-
-    fn execute(&mut self, mut start: isize, mut finish: isize) -> &mut Self{
-        if start != finish {
-            start = self.within_bounds(start);
-            finish = self.within_bounds(finish);
-            let operation = Operation::get(finish, start);
-            match operation{
-                Operation::Add(op) => {
-                    let mut i = start;
-                    loop{
-                        if i == finish { break }
-                        self.driver.execute(self.motions[i as usize].add);
-                        i += op;
-                    }
-                    self.driver.set_status(i);
-                },
-                Operation::Sub(op) => {
-                    let mut i = start;
-                    loop{
-                        if i == finish { break }
-                        i += op;
-                        self.driver.execute(self.motions[i as usize].sub);
-                    }
-                    self.driver.set_status(i);
+fn execute<D: Driver>(driver: &mut D, motions: &Vec<Motion>, mut start: isize, mut finish: isize) {
+    if start != finish {
+        start = within_bounds(motions, start);
+        finish = within_bounds(motions, finish);
+        let operation = Operation::get(finish, start);
+        match operation{
+            Operation::Add(op) => {
+                let mut i = start;
+                loop{
+                    if i == finish { break }
+                    driver.execute(&motions[i as usize].add);
+                    i += op;
                 }
+                driver.set_status(i);
+            },
+            Operation::Sub(op) => {
+                let mut i = start;
+                loop{
+                    if i == finish { break }
+                    i += op;
+                    driver.execute(&motions[i as usize].sub);
+                }
+                driver.set_status(i);
             }
         }
-        self
     }
-    pub fn shift(&mut self, n: isize) -> &mut Self{
-        let start = self.driver.get_status();
-        let finish = self.within_bounds(start + n);
-        self.execute(start, finish);
-        self
-    }
+}
 
-    pub fn goto(&mut self, finish: isize) -> &mut Self{
-        let status = self.driver.get_status();
-        self.execute(status, finish);
-        self
-    }
+pub fn shift<D: Driver>(driver: &mut D, motions: &Vec<Motion>, n: isize) {
+    let start = driver.get_status();
+    let finish = within_bounds(motions, start + n);
+    execute(driver, motions, start, finish);
+}
 
-    pub fn redo(&mut self) -> &mut Self{
-        self.shift(-1);
-        self.shift(1);
-        self
-    }
+pub fn goto<D: Driver>(driver: &mut D, motions: &Vec<Motion>, finish: isize) {
+    let status = driver.get_status();
+    execute(driver, motions, status, finish);
+}
 
-    pub fn up(&mut self) -> &mut Self{
-        let last = self.motions.len() as isize;
-        self.goto(last);
-        self
-    }
+pub fn redo<D: Driver>(driver: &mut D, motions: &Vec<Motion>) {
+    shift(driver, motions, -1);
+    shift(driver, motions, 1);
+}
 
-    pub fn down(&mut self) -> &mut Self{
-        self.goto(0);
-        self
-    }
+pub fn up<D: Driver>(driver: &mut D, motions: &Vec<Motion>) {
+    let last = motions.len() as isize;
+    goto(driver, motions, last);
+}
 
-    pub fn reset(&mut self) -> &mut Self{
-        let status = self.driver.get_status();
-        self.execute(status,0);
-        self.execute(0,status);
-        self
-    }
+pub fn down<D: Driver>(driver: &mut D, motions: &Vec<Motion>) {
+    goto(driver, motions, 0);
+}
 
-    pub fn get_status(&self) -> isize{
-        return self.driver.get_status();
-    }
+pub fn reset<D: Driver>(driver: &mut D, motions: &Vec<Motion>) {
+    let status = driver.get_status();
+    execute(driver, motions, status, 0);
+    execute(driver, motions, 0, status);
 }
 
 pub enum Operation{
