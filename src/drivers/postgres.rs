b8e7ed982d367c8429c5063f8489850cbcbf085e
Victor M. Suarez <svmnotn@gmail.com>
2015-11-01 00:14:19 -0400

    update postgres driver and add reminder to populate it

--- src/drivers/postgres.rs
+++ src/drivers/postgres.rs
@@ -5,23 +5,21 @@ pub struct Driver{
     target: String,
 }
 
+//TODO Implement
 impl DriverTrait for Driver {
     fn new(target: String) -> Self {
         Driver { target: target }
     }
 
     fn get_status(&self) -> isize{
-        println!("Get Status of {}", self.target);
         return 0
     }
 
     fn set_status(&mut self, status: isize) -> &mut Self{
-        println!("Set Status to \'{}\'", status);
         self
     }
 
-    fn execute(&self, motion: &'static str)-> &Self{
-        println!("I am a thing that says \'{0}\' with {1}", motion, self.target);
+    fn execute(&self, motion: &String)-> &Self{
         self
     }
 }
