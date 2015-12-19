b3fce5c36b3edcf83b53f70f34810071e797b8e7
Victor M. Suarez <svmnotn@gmail.com>
2015-12-18 22:39:34 -0500

    begin implementation of motions

--- src/motions.rs
+++ src/motions.rs
@@ -17,29 +17,65 @@ impl Motion {
     }
   }
 }
-
-use std::fs;
-// TODO implement
-pub fn get(directory: String) -> Vec<Motion> {
-    let motions: Vec<Motion> = Vec::new();
-    if let Ok(files) = fs::read_dir(directory.to_string()) {
-        for file in files {
-            let path = file.unwrap().path();
-            println!("Location: {}", path.to_str().unwrap());
-        }
-    } else {
-        panic!("Directory: '{}' not found!", directory);
-    }
-    // TODO Replace this with proper result
-    return vec![Motion::get_test("add 1".to_string(), "sub 1".to_string())];
-}
-
 extern crate regex;
 use self::regex::Regex;
 
-// TODO implement
-pub fn get_template(directory: String) {
-    let template_add = Regex::new("/^([x.]+)([\\-_ ~]+)template\\.add(.*)$/i").unwrap();
-    let template_sub = Regex::new("/^([x.]+)([\\-_ ~]+)template\\.sub(.*)$/i").unwrap();
+struct Template {
+  add_name: String,
+  sub_name: String,
+  version: Vec<isize>,
+  separator: String,
+  extension: String,
+  add: String,
+  sub: String,
+  regex: Regex,
+}
 
+// TODO implement
+impl Template {
+  fn get(names: &Vec<&str>) {
+    let template = Regex::new("/^([x.]+)([\\-_ ~]+)template\\.(add|sub)(.*)$/i").unwrap();
+    let names_iter = names.iter();
+  }
+}
+
+use std::fs;
+
+fn read_dir(directory: &String) -> Vec<String> {
+  let mut names = Vec::new();
+  if let Ok(entries) = fs::read_dir(directory.to_string()) {
+    for entry in entries {
+      if let Ok(file_name) = entry.unwrap().file_name().into_string() {
+        names.push(file_name);
+      } else {
+        panic!("File Name did not contain valid Unicode data");
+      }
+    }
+  } else {
+    panic!("Directory: '{}' not found!", directory);
+  }
+  names
+}
+
+/*
+ pub fn discover(directory: &String) -> Vec<Motion>{
+  let names = read_dir(directory);
+  let template = Template::get(&names);
+
+ }
+*/
+
+// TODO implement
+pub fn get(directory: &String) -> Vec<Motion> {
+  let motions: Vec<Motion> = Vec::new();
+  if let Ok(files) = fs::read_dir(directory.to_string()) {
+    for file in files {
+      let path = file.unwrap().file_name();
+      println!("Location: {}", path.to_str().unwrap());
+    }
+  } else {
+    panic!("Directory: '{}' not found!", directory);
+  }
+  // TODO Replace this with proper result
+  return vec![Motion::get_test("add 1".to_string(), "sub 1".to_string())];
 }
