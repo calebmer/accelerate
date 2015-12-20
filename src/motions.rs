d2e3040cf2d89a88cc2cd166d6831b5201a48dbd
Victor M. Suarez <svmnotn@gmail.com>
2015-12-20 00:11:58 -0500

    compiles!

--- src/motions.rs
+++ src/motions.rs
@@ -35,10 +35,25 @@ struct Template {
 
 // TODO implement
 impl Template {
-  fn get(names: &Vec<&str>) {
-    let template = Regex::new("/^([x.]+)([\\-_ ~]+)template\\.(add|sub)(.*)$/i").unwrap();
-    let names_iter = names.iter();
+  fn get(names: &Vec<String>) {
+    let template_add = Regex::new("/^([x.]+)([\\-_ ~]+)template\\.add(.*)$/i").unwrap();
+    let template_sub = Regex::new("/^([x.]+)([\\-_ ~]+)template\\.sub(.*)$/i").unwrap();
+    let mut names_iter = names.iter();
+    let add_name = names_iter.find(|name| template_add.is_match(name)).unwrap();
+    let sub_name = names_iter.find(|name| template_sub.is_match(name)).unwrap();
 
+    if template_add.replace_all(add_name, "$1,$2,$3") != template_sub.replace_all(sub_name, "$1,$2,$3") {
+      panic!("And and sub template names do not equal each other")
+    }
+
+    let captures = template_add.captures(add_name).unwrap();
+    let version = captures.at(0).unwrap().split('.').map(|s| s.len());
+    let seperator = captures.at(1).unwrap();
+    let extension = captures.at(2).unwrap();
+
+    // x.xx.xxx => [1, 2, 3] => ["\\d{1}", "\\d{2}", "\\d{3}"] => "\\.\\d{1}\\.\\d{2}\\.\\d{3}"
+    let version_regex = version.map(|len| "\\d{".to_string() + &len.to_string() + "}").fold("".to_string(), |acc, re| (acc + "\\." + &re));
+    let template_regex = Regex::new(&("/^(".to_string() + &version_regex + ")"));
   }
 }
 
@@ -49,7 +64,7 @@ fn read_dir(directory: &str) -> Vec<String> {
       if let Ok(file_name) = entry.unwrap().file_name().into_string() {
         names.push(file_name);
       } else {
-        panic!("File Name did not contain valid Unicode data");
+        panic!("File name did not contain valid Unicode data");
       }
     }
   } else {
