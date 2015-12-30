2b18d121fa83eb5859d167155de9b43a2a395edc
Victor M. Suarez <svmnotn@gmail.com>
2015-12-30 13:43:47 -0500

    impl gated subcommands so that deletions would have to be accepted

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,9 @@ mod tests;
 
 #[macro_use]
 extern crate clap;
-use clap::{App, SubCommand};
+use clap::{App, SubCommand, ArgMatches};
+use std::io::*;
+use std::process;
 
 use drivers::Driver;
 use motions::Motion;
@@ -22,7 +24,8 @@ fn main() {
                   .author("Caleb Meredith <calebmeredith8@gmail.com>\nVictor M. Suarez <svmnotn@gmail.com>")
                   .about("Accelerate back and forth through time for your database or other in-place systems")
                   .args_from_usage("--target=<url>     -t 'the targeted url to accelerate'
-                                    --directory=[path] -d 'the directory holding the motions (defaults to the current dir)'")
+                                    --directory=[path] -d 'the directory holding the motions (defaults to the current dir)'
+                                    --yes              -y 'auto accept the changes, used for subcommands that remove information (USE WITH CAUTION!)'")
                   .subcommand(SubCommand::with_name("redo").about("subtracts then adds the last motion"))
                   .subcommand(SubCommand::with_name("up").about("adds all remaining motions"))
                   .subcommand(SubCommand::with_name("down").about("subtracts all previous motions"))
@@ -56,15 +59,43 @@ fn main() {
   // Go through and find what matched
   match matches.subcommand() {
     ("up", Some(_)) => accelerator::up(&mut driver, &mots),
-    ("down", Some(_)) => accelerator::down(&mut driver, &mots),
-    ("redo", Some(_)) => accelerator::redo(&mut driver, &mots),
-    ("reset", Some(_)) => accelerator::reset(&mut driver, &mots),
     ("ls", Some(m)) => ls(directory, mots, m.is_present("long")),
     ("create", Some(m)) => motions::create(directory, mots, value_t_or_exit!(m.value_of("name"), String)),
     ("add", Some(m)) => accelerator::shift(&mut driver, &mots, value_t!(m.value_of("n"), isize).unwrap_or(1)),
-    ("sub", Some(m)) => accelerator::shift(&mut driver, &mots, value_t!(m.value_of("n"), isize).unwrap_or(1) * -1),
-    ("shift", Some(m)) => accelerator::shift(&mut driver, &mots, value_t_or_exit!(m.value_of("n"), isize)),
-    ("goto", Some(m)) => accelerator::goto(&mut driver, &mots, value_t_or_exit!(m.value_of("n"), isize)),
+    (cmd, Some(m)) => gate((cmd, m), driver, mots, matches.is_present("yes")),
+    _ => println!("Nothing to do!\nRe-run with --help for more information"),
+  }
+}
+
+fn gate(matches: (&str, &ArgMatches), mut driver: Box<Driver>, mots: Vec<Motion>, gate: bool) {
+  if !gate {
+    println!("You might remove information by doing this action.\nDo you wish to continue? (Y/N)");
+    let stdin = stdin();
+    for line in stdin.lock().lines() {
+      let s: &str = &line.unwrap();
+      match s {
+        "Yes" | "yes" | "Y" | "y" | "yup" => {
+          println!("Continuing at your own risk.");
+          break;
+        }
+        "No" | "no" | "N" | "n" | "nope" => {
+          println!("Exiting.");
+          process::exit(0);
+        }
+        _ => {
+          println!("Could not understand response, Exiting.");
+          process::exit(0);
+        }
+      }
+    }
+  }
+  match matches {
+    ("down", _) => accelerator::down(&mut driver, &mots),
+    ("redo", _) => accelerator::redo(&mut driver, &mots),
+    ("reset", _) => accelerator::reset(&mut driver, &mots),
+    ("sub", m) => accelerator::shift(&mut driver, &mots, value_t!(m.value_of("n"), isize).unwrap_or(1) * -1),
+    ("shift", m) => accelerator::shift(&mut driver, &mots, value_t_or_exit!(m.value_of("n"), isize)),
+    ("goto", m) => accelerator::goto(&mut driver, &mots, value_t_or_exit!(m.value_of("n"), isize)),
     _ => println!("Nothing to do!\nRe-run with --help for more information"),
   }
 }
