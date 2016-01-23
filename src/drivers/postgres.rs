2bf77e2c563ff0270ed7ee7b8791417b1183e2ba
calebmer <calebmeredith8@gmail.com>
2016-01-23 14:10:18 -0500

    Rename postgres driver

--- src/drivers/postgres.rs
+++ src/drivers/postgres.rs
@@ -1,16 +1,16 @@
 #![cfg(feature = "driver-postgres")]
-use drivers::Driver as DriverTrait;
+use drivers::Driver;
 
-pub struct Driver {
+pub struct PostgresDriver {
   target: String,
 }
 
-impl Driver {
-  fn new(target: String) -> Self { Driver { target: target } }
+impl PostgresDriver {
+  fn new(target: String) -> Self { PostgresDriver { target: target } }
 }
 
 // TODO Implement
-impl DriverTrait for Driver {
+impl Driver for PostgresDriver {
   fn get_status(&self) -> isize { return 0; }
 
   fn set_status(&mut self, status: isize) {}
